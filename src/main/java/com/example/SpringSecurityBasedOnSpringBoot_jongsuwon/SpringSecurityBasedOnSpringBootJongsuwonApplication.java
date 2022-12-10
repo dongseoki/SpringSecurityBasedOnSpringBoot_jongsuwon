6d6b80bb84f6fb2cb10e449af9d51ade53ff51a3
Dong Seok Lee <dongseok.lee.log@gmail.com>
2022-12-10 20:13:45 +0900

    first commit - 스프링 세큐리티 프로젝트 구성

---
+++ src/main/java/com/example/SpringSecurityBasedOnSpringBoot_jongsuwon/SpringSecurityBasedOnSpringBootJongsuwonApplication.java
@@ -0,0 +1,13 @@
+package com.example.SpringSecurityBasedOnSpringBoot_jongsuwon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityBasedOnSpringBootJongsuwonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityBasedOnSpringBootJongsuwonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SpringSecurityBasedOnSpringBoot_jongsuwon/SpringSecurityBasedOnSpringBootJongsuwonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringSecurityBasedOnSpringBoot_jongsuwon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityBasedOnSpringBootJongsuwonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
