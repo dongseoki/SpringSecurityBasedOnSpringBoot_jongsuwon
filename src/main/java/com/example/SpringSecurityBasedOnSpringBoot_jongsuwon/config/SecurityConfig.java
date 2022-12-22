6e60fc915811eb33ebf718e405b9afb260489de1
Dong Seok Lee <dongseok.lee.log@gmail.com>
2022-12-22 20:23:49 +0900

    섹션1.9) 동시 세션 제어, 세션 고정 보호, 세션 정책에 대해서 공부함.

--- src/main/java/com/example/SpringSecurityBasedOnSpringBoot_jongsuwon/config/SecurityConfig.java
+++ src/main/java/com/example/SpringSecurityBasedOnSpringBoot_jongsuwon/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -77,7 +78,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 /* user 계정 확인 메서드 */
                 .userDetailsService(userDetailsService);
 
-
+        http.sessionManagement()
+                .maximumSessions(1)
+                .maxSessionsPreventsLogin(false)
+                .and()
+            .sessionFixation().changeSessionId()
+            .sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED);
 
     }
 
